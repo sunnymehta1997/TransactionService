6f614fe705d31c27085f85f5b4c4b27bb6f7841e
NiranjanRaut <nirranjan.raut@gmail.com>
2022-03-26 15:48:38 +0530

    API Controller test completed.

--- src/test/java/org/hcl/hackathon/transactions/util/APICallerTest.java
+++ src/test/java/org/hcl/hackathon/transactions/util/APICallerTest.java
@@ -1,42 +1,53 @@
 package org.hcl.hackathon.transactions.util;
 
 import org.hcl.hackathon.transactions.dto.TransactionDetailsDTO;
-import org.junit.Before;
+import org.hcl.hackathon.transactions.service.impl.TransactionDetailsServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class APICallerTest {
 
     @Mock
-    private ExecutorService executorService;
+    private ExecutorService mockExecutorService;
+    @Mock
+    private TransactionDetailsServiceImpl mockService;
 
     @InjectMocks
-    private APICaller caller;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.openMocks(this);
-    }
+    private APICaller apiCallerUnderTest;
 
     @Test
-    public void submitRequestForTransaction() {
-        UUID uuid = UUID.randomUUID();
-        TransactionDetailsDTO dto = new TransactionDetailsDTO();
-        caller.submitRequestForTransaction(uuid, dto);
-        assertNotNull(caller);
-    }
+    public void testSubmitRequestForTransaction() {
+        // Setup
+        final TransactionDetailsDTO dto = new TransactionDetailsDTO();
+        dto.setTransactionID(UUID.fromString("c33a6a84-53ff-45b6-8157-6da8194efd25"));
+        dto.setTransactionDate(LocalDateTime.of(2020, 1, 1, 0, 0, 0));
+        dto.setStatus("status");
+        dto.setOrderID("orderID");
+        dto.setOrderReferenceNumber(0);
 
+        doAnswer(invocation -> {
+            ((Runnable) invocation.getArguments()[0]).run();
+            return null;
+        }).when(mockExecutorService).execute(any(Runnable.class));
+
+        // Run the test
+        apiCallerUnderTest.submitRequestForTransaction(UUID.fromString("c33a6a84-53ff-45b6-8157-6da8194efd25"), dto);
+
+        // Verify the results
+        verify(mockExecutorService).execute(any(Runnable.class));
+        verify(mockService).update(eq(UUID.fromString("c33a6a84-53ff-45b6-8157-6da8194efd25")), any(TransactionDetailsDTO.class));
+    }
 }
